542668f26d70ced1d9c00b70ca5a60002e979e78
rowan-gos <rowan.phan.gos@gmail.com>
2026-03-09 16:43:24 +0700

    feat: update AuthModule to include Passport and JWT strategies for enhanced authentication

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,10 +2,24 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './strategies/local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies/JWT.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
-  imports: [UserModule],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  imports: [
+    UserModule,
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'default-secret',
+      signOptions: {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        expiresIn: (process.env.JWT_EXPIRES_IN || '15m') as any,
+      },
+    }),
+  ],
 })
 export class AuthModule {}
